5c0e147787e4614e3bbe301f3ffcb460574bd539
Igor Marchi <igoraugustomarchi@gmail.com>
2023-03-07 23:31:38 -0300

    feat: create update product use case

---
+++ src/usecase/product/update/update.product.integration.spec.ts
@@ -0,0 +1,82 @@
+import { Sequelize } from "sequelize-typescript";
+import { v4 as uuid } from "uuid";
+import Product from "../../../domain/product/entity/product";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import {
+  InputUpdateProductDto,
+  OutputUpdateProductDto,
+} from "./update.product.dto";
+import UpdateProductUseCase from "./update.product.usecase";
+
+describe("Test update product use case", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should update a product", async () => {
+    const productRepository = new ProductRepository();
+    const useCase = new UpdateProductUseCase(productRepository);
+    const product = new Product(uuid(), "ball", 10);
+    productRepository.create(product);
+
+    const input: InputUpdateProductDto = {
+      id: product.id,
+      name: "ball updated",
+      price: 20,
+    };
+    const output: OutputUpdateProductDto = {
+      id: product.id,
+      name: input.name,
+      price: input.price,
+    };
+    const result = await useCase.execute(input);
+    expect(result).toEqual(output);
+  });
+
+  it("should thrown an error when name is missing", async () => {
+    const productRepository = new ProductRepository();
+    const useCase = new UpdateProductUseCase(productRepository);
+    const product = new Product(uuid(), "ball", 10);
+    productRepository.create(product);
+
+    const input: InputUpdateProductDto = {
+      id: product.id,
+      name: "",
+      price: 10,
+    };
+    await expect(useCase.execute(input)).rejects.toThrow(
+      new Error("Name is required")
+    );
+  });
+
+  it("should thrown an error when price is less than zero", async () => {
+    const productRepository = new ProductRepository();
+    const useCase = new UpdateProductUseCase(productRepository);
+    const product = new Product(uuid(), "ball", 10);
+    productRepository.create(product);
+
+    const input: InputUpdateProductDto = {
+      id: product.id,
+      name: "ball",
+      price: -10,
+    };
+    await expect(useCase.execute(input)).rejects.toThrow(
+      new Error("Price must be greater than zero")
+    );
+  });
+});
---
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -0,0 +1,63 @@
+import { v4 as uuid } from "uuid";
+import Product from "../../../domain/product/entity/product";
+import {
+  InputUpdateProductDto,
+  OutputUpdateProductDto,
+} from "./update.product.dto";
+import UpdateProductUseCase from "./update.product.usecase";
+
+const product = new Product(uuid(), "ball", 10);
+
+const MockRepository = () => {
+  return {
+    find: jest.fn().mockReturnValue(Promise.resolve(product)),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  };
+};
+
+describe("Unit test update product use case", () => {
+  it("should update a product", async () => {
+    const productRepository = MockRepository();
+    const useCase = new UpdateProductUseCase(productRepository);
+    const input: InputUpdateProductDto = {
+      id: product.id,
+      name: "ball updated",
+      price: 20,
+    };
+    const output: OutputUpdateProductDto = {
+      id: product.id,
+      name: input.name,
+      price: input.price,
+    };
+    const result = await useCase.execute(input);
+    expect(result).toEqual(output);
+  });
+
+  it("should thrown an error when name is missing", async () => {
+    const productRepository = MockRepository();
+    const useCase = new UpdateProductUseCase(productRepository);
+    const input: InputUpdateProductDto = {
+      id: product.id,
+      name: "",
+      price: 20,
+    };
+    await expect(useCase.execute(input)).rejects.toThrowError(
+      new Error("Name is required")
+    );
+  });
+
+  it("should thrown an error when price is less than zero", async () => {
+    const productRepository = MockRepository();
+    const useCase = new UpdateProductUseCase(productRepository);
+    const input: InputUpdateProductDto = {
+      id: product.id,
+      name: "ball",
+      price: -1,
+    };
+    await expect(useCase.execute(input)).rejects.toThrowError(
+      new Error("Price must be greater than zero")
+    );
+  });
+});
