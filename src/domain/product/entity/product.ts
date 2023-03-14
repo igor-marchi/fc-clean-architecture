474bf8da836695531bd969fef8116e1be83a788c
Igor Marchi <igoraugustomarchi@gmail.com>
2023-03-13 21:07:41 -0300

    feat: :sparkles: use factory patter to validate de entity

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,10 +1,9 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
+import ProductValidatorFactory from "../factory/product.validator.factory";
 import ProductInterface from "./product.interface";
 
 export default class Product extends Entity implements ProductInterface {
-  static readonly CONTEXT = "Product";
-
   private _name: string;
   private _price: number;
 
@@ -34,28 +33,11 @@ export default class Product extends Entity implements ProductInterface {
     this.validate();
   }
 
-  validate(): boolean {
-    if (this._id.length === 0) {
-      this.notification.addError({
-        context: Product.CONTEXT,
-        message: "Id is required",
-      });
-    }
-    if (this._name.length === 0) {
-      this.notification.addError({
-        context: Product.CONTEXT,
-        message: "Name is required",
-      });
-    }
-    if (this._price < 0) {
-      this.notification.addError({
-        context: Product.CONTEXT,
-        message: "Price must be greater than zero",
-      });
-    }
+  validate() {
+    ProductValidatorFactory.create().validate(this);
+
     if (this.notification.hasErrors()) {
       throw new NotificationError(this.notification.getErrors());
     }
-    return true;
   }
 }
